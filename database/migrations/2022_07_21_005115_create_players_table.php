b38505e9231ed3af09138ffdc7d92d87e7993f31
Jennifer <jeante05@gmail.com>
2022-07-20 20:00:39 -0500

    install auth

--- database/migrations/2022_07_21_005115_create_players_table.php
+++ database/migrations/2022_07_21_005115_create_players_table.php
@@ -15,6 +15,8 @@ class CreatePlayersTable extends Migration
     {
         Schema::create('players', function (Blueprint $table) {
             $table->id();
+            $table->string('name',100);
+            $table->string('url',1000);
             $table->timestamps();
         });
     }
