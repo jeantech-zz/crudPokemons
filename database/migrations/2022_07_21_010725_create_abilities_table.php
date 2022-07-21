316dda8a8ca2248d6726a71d390526dab85100d2
Jennifer <jeante05@gmail.com>
2022-07-20 20:25:13 -0500

    update clases migration

--- database/migrations/2022_07_21_010725_create_abilities_table.php
+++ database/migrations/2022_07_21_010725_create_abilities_table.php
@@ -6,12 +6,7 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateAbilitiesTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
+    public function up():void
     {
         Schema::create('abilities', function (Blueprint $table) {
             $table->id();
@@ -19,12 +14,7 @@ class CreateAbilitiesTable extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
+    public function down():void
     {
         Schema::dropIfExists('abilities');
     }
